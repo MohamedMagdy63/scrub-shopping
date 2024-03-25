51ba138fec2bd83bd9edef4c4be899e370aed722
Mohamed Magdy <mohammedmagdy6522@gmail.com>
2024-03-26 00:27:40 +0200

    Size Guide

--- src/Components/sizeGuide.jsx
+++ src/Components/sizeGuide.jsx
@@ -1,7 +1,6 @@
 import Navbar from '../Components/Navbar';
 import Footer from '../Components/Footer';
 import React, { useEffect } from 'react';
-import Hindawi from '../Data/Images/AboutData/Hindawi.JPG'
 import WomenSize from '../Data/Images/AboutData/WomenSize.PNG'
 import MenSize from '../Data/Images/AboutData/MenSize.PNG'
 
@@ -15,20 +14,17 @@ function SizeGuide() {
   return (
     // container
     <div>
-    <Navbar />
-    {/* Container */}
-    
-
-    <h1 className="text-2xl font-semibold text-gray-900 text-center mt-5">
-      Sizes for Men & Women
-    </h1>
-    <div className="flex flex-col md:flex-row">
-      <img className="w-full h-2/4 md:w-1/2 p-5" src={WomenSize} alt="" />
-      <img className="w-full h-2/4 md:w-1/2 p-5" src={MenSize} alt="" />
+      <Navbar />
+      {/* Container */}
+      <h1 className="text-2xl font-semibold text-gray-900 text-center mt-5">
+        Sizes for Men & Women
+      </h1>
+      <div className="flex flex-col md:flex-row">
+        <img className="w-2/4 h-2/4 md:w-1/2 p-5" src={WomenSize} alt="" />
+        <img className="w-2/4 h-2/4 md:w-1/2 p-5" src={MenSize} alt="" />
+      </div>
+      <Footer />
     </div>
-
-    <Footer />
-  </div>
   );
 }
 
